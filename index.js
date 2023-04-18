48e5cb40af31662e1b6d13b94c91e36d524022bf
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-18 17:40:36 -0300

    add assigment driver script to index

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 import { Tree } from './modules/binarySearchTree.js';
 
-let test = Tree(2, 1, 3, 4, 5, 2, 6);
+const randomValue = () => {
+  let arr = [];
+  for (let i = 0; i < 15; i += 1) {
+    arr.push(Math.floor(Math.random() * 1000));
+  }
+  return arr;
+};
+
+let test = Tree(randomValue());
 
 test.buildTree();
-console.log(test.getArr());
+console.log(test.isBalanced());
+console.log(test.levelOrder());
+console.log(test.inOrder());
+console.log(test.preOrder());
+console.log(test.preOrder());
+
+for (let j = 0; j < 200; j += 1) {
+  test.insert(Math.floor(Math.random() * 1000));
+}
+
+test.show();
+console.log(test.isBalanced());
+console.log('--------------------------');
+console.log('--------------------------');
+test.reBalance();
+console.log(test.isBalanced());
